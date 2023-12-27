144d85e9d604e04cb0382a325bd8d67dc659ad9c
sengokyu <sengokyu+gh@gmail.com>
2023-12-27 23:35:38 +0900

    Update bin

--- bin/wordpress-container.ts
+++ bin/wordpress-container.ts
@@ -1,21 +1,16 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { WordpressContainerStack } from '../lib/wordpress-container-stack';
+import * as cdk from "aws-cdk-lib";
+import { Ec2MinimumStage } from "../lib/ec2-minimum-stage";
+import { FargateAuroraEfsStage } from "../lib/fargate-aurora-efs-stage";
+
+const env = {
+  region: process.env.CDK_DEFAULT_REGION,
+};
 
 const app = new cdk.App();
-new WordpressContainerStack(app, 'WordpressContainerStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+// サンプル1 Ec2
+new Ec2MinimumStage(app, "Ec2MinimumStage", { env });
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
-
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+// サンプル2 Aurora / EFS
+new FargateAuroraEfsStage(app, "FargateAuroraEfsStage", { env });
